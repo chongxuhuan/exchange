c0bf3b2e73fbf182f953eb05708394d605114713
fangweng <cenwenchu@163.com>
2012-08-01 12:29:16 +0800

    update

--- src/java/com/taobao/exchange/relation/AccountZoo.java
+++ src/java/com/taobao/exchange/relation/AccountZoo.java
@@ -8,6 +8,9 @@ import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import org.apache.commons.lang.StringUtils;
+import com.taobao.exchange.util.ServiceException;
+
 
 /**
  * 卖家用户，拥有一个二手平台帐号和一个或或者多个关系网站帐号
@@ -22,6 +25,8 @@ public class AccountZoo implements java.io.Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 3370983959338992307L;
+	
+	public static String split = "\"||\"";
 
 	User secondhandAccount;//二手交易平台身份
 	private ConcurrentMap<String,User> relationAccounts;//多个社会化关系开放平台身份
@@ -29,6 +34,37 @@ public class AccountZoo implements java.io.Serializable{
 	public AccountZoo()
 	{	
 		relationAccounts = new ConcurrentHashMap<String,User>();
+	}
+	
+	public AccountZoo(String az) throws ServiceException
+	{
+		relationAccounts = new ConcurrentHashMap<String,User>();
+		
+		if (az.indexOf(split) > 0)
+		{
+			String[] users = StringUtils.splitByWholeSeparator(az, split);
+			
+			if (!users[0].equals("null"))
+			{
+				secondhandAccount = new User(users[0]);
+			}
+			
+			if (users.length > 1)
+			{
+				for (int i = 1; i < users.length; i++)
+				{
+					if (users[i].indexOf(User.split) < 0)
+						continue;
+						
+					User u = new User(users[i]);
+					relationAccounts.put(u.generateUserKey(), u);
+				}
+			}
+			
+		}
+		else
+			throw new ServiceException("User string format error!");
+		
 	}
 
 	public User getSecondhandAccount() {
@@ -59,4 +95,55 @@ public class AccountZoo implements java.io.Serializable{
 		relationAccounts.clear();
 	}
 
+	@Override
+	public String toString()
+	{
+		StringBuilder result = new StringBuilder();
+		
+		if (secondhandAccount != null)
+			result.append(secondhandAccount.toString());
+		else
+			result.append("null");
+		
+		result.append(split);
+		
+		
+		
+		for (User r : relationAccounts.values())
+		{
+			result.append(r.toString()).append(split);
+		}
+		
+		return result.toString();
+	}
+	
+	
+	public static void main(String[] args) throws ServiceException
+	{
+		AccountZoo az = new AccountZoo();
+//		User s = new User();
+//		s.setId("sfafda");
+//		s.setPlatformId("taobao");
+//		az.setSecondhandAccount(s);
+		
+		User r1 = new User();
+		r1.setId("1231312");
+		r1.setPlatformId("sina");
+		az.storeRelation(r1);
+		
+		User r2 = new User();
+		r2.setId("12313adsf12");
+		r2.setPlatformId("tencent");
+		r2.setName("fangweng");
+		az.storeRelation(r2);
+		
+		String ss = az.toString();
+		
+		System.out.println(ss);
+		
+		AccountZoo az1 = new AccountZoo(ss);
+		System.out.println(az1.toString());
+		
+	}
+	
 }
--- src/java/com/taobao/exchange/relation/User.java
+++ src/java/com/taobao/exchange/relation/User.java
@@ -3,8 +3,11 @@
  */
 package com.taobao.exchange.relation;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.taobao.exchange.app.AppAuthEntity;
 import com.taobao.exchange.util.AppClientUtil;
+import com.taobao.exchange.util.ServiceException;
 
 /**
  * 关系平台的用户信息
@@ -23,14 +26,32 @@ public class User implements java.io.Serializable{
 	private String id;//用户id
 	private String name;//用户名称
 	
+	public static String split = "\"-\"";
+	
 	public User()
 	{
 		super();
 	}
 	
+	public User(String u) throws ServiceException
+	{
+		if (u.indexOf(split) > 0)
+		{
+			String[] us = StringUtils.splitByWholeSeparator(u, split);
+			
+			this.setPlatformId(us[0]);
+			this.setId(us[1]);
+			
+			if (!us[2].equals("null"))
+				this.setName(us[2]);
+			
+		}
+		else
+			throw new ServiceException("User string format error!");
+	}
+	
 	public User(AppAuthEntity authEntity)
 	{
-		super();
 		this.platformId = authEntity.getPlatformId();
 		this.id = authEntity.getUid();
 		this.name = authEntity.getNick();
@@ -58,5 +79,28 @@ public class User implements java.io.Serializable{
 	public String generateUserKey()
 	{
 		return AppClientUtil.generatePlatformUUID(platformId, id);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return new StringBuilder().append(platformId)
+				.append(split).append(id).append(split).append(name).toString();
+	}
+	
+	public static void main(String[] args) throws ServiceException
+	{
+		User t = new User();
+		t.setId("1221");
+		t.setPlatformId("sdfas");
+		
+		String a = t.toString();
+		
+		System.out.println(a.toString());
+		
+		User t1 = new User(a);
+		
+		System.out.println(t1.toString());
+		
 	}
 }
